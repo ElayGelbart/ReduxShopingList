6c44777237350d69449e4149284a58596e3d0acd
ElayGelbart <elaygelbart@gmail.com>
2021-12-20 14:51:55 +0200

    wORKING Redux

---
+++ my-app/src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProductList from "./ProductList";
+export default function App() {
+  return (
+    <div>
+      <h1>ShopList</h1>
+      <ProductList />
+    </div>
+  );
+}
